046e7d1af5d4768f8101ad91f829c6cd2340c8ef
Artur Matukevich <malikharik@gmail.com>
2026-02-26 13:52:19 +0300

    fix

--- lib/core/utils/secure_storage.dart
+++ lib/core/utils/secure_storage.dart
@@ -1,27 +1,29 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:search_frontend/core/constants/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SecureStorage {
-  final _storage = FlutterSecureStorage();
-
   Future<void> saveTokens({
     required String accessToken,
     required String refreshToken,
   }) async {
-    await _storage.write(key: ACCESS_TOKEN_STORAGE_KEY, value: accessToken);
-    await _storage.write(key: REFRESH_TOKEN_STORAGE_KEY, value: refreshToken);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(ACCESS_TOKEN_STORAGE_KEY, accessToken);
+    await prefs.setString(REFRESH_TOKEN_STORAGE_KEY, refreshToken);
   }
 
   Future<String?> getAccessToken() async {
-    return await _storage.read(key: ACCESS_TOKEN_STORAGE_KEY);
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(ACCESS_TOKEN_STORAGE_KEY);
   }
 
   Future<String?> getRefreshToken() async {
-    return await _storage.read(key: REFRESH_TOKEN_STORAGE_KEY);
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(REFRESH_TOKEN_STORAGE_KEY);
   }
 
   Future<void> clearTokens() async {
-    await _storage.delete(key: ACCESS_TOKEN_STORAGE_KEY);
-    await _storage.delete(key: REFRESH_TOKEN_STORAGE_KEY);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(ACCESS_TOKEN_STORAGE_KEY);
+    await prefs.remove(REFRESH_TOKEN_STORAGE_KEY);
   }
 }
